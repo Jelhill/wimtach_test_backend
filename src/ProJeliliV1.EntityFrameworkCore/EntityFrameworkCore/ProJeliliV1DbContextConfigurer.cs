12e2b393efc0b228c7586e48d1cd8065f76e469a
Jelili Umaru <talk2jelili2000@gmail.com>
2024-09-19 01:40:52 -0400

    Replace Mssql Setup with Postgres Setup

--- src/ProJeliliV1.EntityFrameworkCore/EntityFrameworkCore/ProJeliliV1DbContextConfigurer.cs
+++ src/ProJeliliV1.EntityFrameworkCore/EntityFrameworkCore/ProJeliliV1DbContextConfigurer.cs
@@ -7,12 +7,12 @@ namespace ProJeliliV1.EntityFrameworkCore
     {
         public static void Configure(DbContextOptionsBuilder<ProJeliliV1DbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString);
+            builder.UseNpgsql(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<ProJeliliV1DbContext> builder, DbConnection connection)
         {
-            builder.UseSqlServer(connection);
+            builder.UseNpgsql(connection);
         }
     }
 }
--- src/ProJeliliV1.Web.Host/Startup/ProJeliliV1WebHostModule.cs
+++ src/ProJeliliV1.Web.Host/Startup/ProJeliliV1WebHostModule.cs
@@ -23,5 +23,13 @@ namespace ProJeliliV1.Web.Host.Startup
         {
             IocManager.RegisterAssemblyByConvention(typeof(ProJeliliV1WebHostModule).GetAssembly());
         }
+
+        public override void PreInitialize()
+        {
+            System.AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+            System.AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            
+            // https://www.npgsql.org/efcore/release-notes/6.0.html?tabs=annotations
+        }
     }
 }
