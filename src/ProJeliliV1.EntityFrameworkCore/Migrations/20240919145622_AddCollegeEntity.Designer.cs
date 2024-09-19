b5d0e2f4f4d9c0a53073ba839d42c668664435b6
Jelili Umaru <talk2jelili2000@gmail.com>
2024-09-19 10:58:04 -0400

    Created College table through Migrations

--- src/ProJeliliV1.EntityFrameworkCore/Migrations/20240919145622_AddCollegeEntity.Designer.cs
+++ src/ProJeliliV1.EntityFrameworkCore/Migrations/20240919145622_AddCollegeEntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ProJeliliV1.EntityFrameworkCore;
@@ -11,9 +12,11 @@ using ProJeliliV1.EntityFrameworkCore;
 namespace ProJeliliV1.Migrations
 {
     [DbContext(typeof(ProJeliliV1DbContext))]
-    partial class ProJeliliV1DbContextModelSnapshot : ModelSnapshot
+    [Migration("20240919145622_AddCollegeEntity")]
+    partial class AddCollegeEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ProJeliliV1.EntityFrameworkCore/Migrations/20240919145622_AddCollegeEntity.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ProJeliliV1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCollegeEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CollegeId",
+                table: "Students",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Colleges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    GPSLatitude = table.Column<double>(type: "double precision", nullable: false),
+                    GPSLongitude = table.Column<double>(type: "double precision", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    JeliliString = table.Column<string>(type: "text", nullable: true),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colleges", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_CollegeId",
+                table: "Students",
+                column: "CollegeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Colleges_CollegeId",
+                table: "Students",
+                column: "CollegeId",
+                principalTable: "Colleges",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Colleges_CollegeId",
+                table: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Colleges");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_CollegeId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CollegeId",
+                table: "Students");
+        }
+    }
+}
